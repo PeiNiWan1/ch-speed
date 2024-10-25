bfce3471daf358b422b1b9bb31c52f988f6c943a
pei <1617436947@qq.com>
2024-10-25 15:43:12 +0800

    添加系列回去默认展示类型

--- ChSpeed/serializer.py
+++ ChSpeed/serializer.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from rest_framework.serializers import ModelSerializer,ListSerializer,BaseSerializer
+from rest_framework.fields import empty
 class DisplayType(Enum):
     LIST = "LIST"
     FOREIGN = "FOREIGN"
@@ -8,7 +9,6 @@ class BaseSerializer(ModelSerializer):
 
   # 展示类型
   display_type = None
-
   # 敏感字段，不会被序列化
   sensitive_fields = []
   # 标记作为外键时引用需要排除的字段
@@ -16,8 +16,12 @@ class BaseSerializer(ModelSerializer):
   # 标记作为列表展示时需要排除的字段
   exclude_list = []
 
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+  def __init__(self,instance=None,data=empty, **kwargs):
+    super().__init__(instance=None,data=empty, **kwargs)
+
+    if isinstance(instance,list) and self.display_type==None:
+      self.display_type = DisplayType.LIST
+
     for field_name in self.sensitive_fields:
       self.fields.pop(field_name)
     if self.display_type == DisplayType.LIST:
