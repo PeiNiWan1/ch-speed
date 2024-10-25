14e4792b1e97356268ec89bb12d7a955752c8ec6
pei <1617436947@qq.com>
2024-10-25 10:56:00 +0800

    添加权限校验模块

---
+++ ChSpeed/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import BasePermission
+# 判断是否拥有特殊权限
+class HasPerm(BasePermission):
+    """
+    检查单独权限，或分组中权限
+    """
+    def __init__(self,perm) -> None:
+        self.perm=perm
+        super().__init__()
+
+    def has_permission(self, request, view):
+        # 检查用户权限中是否拥有perm权限
+        user = request.user
+        allPerm= user.get_all_permissions()
+        print("检查权限",self.perm,self.perm in allPerm,allPerm)
+        if self.perm in allPerm:
+            return True
+        return False
