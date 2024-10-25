53c747699bad22c6567853cdf1e8a0e85fc6d93c
pei <1617436947@qq.com>
2024-10-25 10:56:22 +0800

    重构继承关系

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth import get_user_model
 from rest_framework.filters import SearchFilter
+from ChSpeed.viewsets import CommonViewSetModel
 from base.serializers import UserSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -9,7 +10,7 @@ from rest_framework import generics
 from knox.models import AuthToken
 from ChSpeed.loggers import Loggers
 
-class UserView(ModelViewSet):
+class UserView(CommonViewSetModel):
     queryset = get_user_model().objects.all()
     serializer_class=UserSerializer
     filter_backends = [SearchFilter]
@@ -21,7 +22,6 @@ class UserView(ModelViewSet):
         return Response({"user":"awdawdaw"})
 class LoginView(generics.GenericAPIView):
     serializer_class = UserSerializer
-
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -32,6 +32,9 @@ class LoginView(generics.GenericAPIView):
         if not user.check_password(serializer.data['password']):
             Loggers.info(serializer.data['phone'],"密码错误")
             return Response({"msg":"密码错误","code":4004})
+        if not user.is_active:
+            Loggers.info(serializer.data['phone'],"用户被禁用")
+            return Response({"msg":"用户被禁用","code":4004})
         _, token = AuthToken.objects.create(user)
         return Response({
             "user": self.get_serializer(user).data,
