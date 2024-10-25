03f5258fea2892dadac742d62a5f640c6445c474
pei <1617436947@qq.com>
2024-10-25 15:36:00 +0800

    添加基础序列化器

---
+++ ChSpeed/serializer.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from rest_framework.serializers import ModelSerializer,ListSerializer,BaseSerializer
+class DisplayType(Enum):
+    LIST = "LIST"
+    FOREIGN = "FOREIGN"
+    DETAIL = "DETAIL"
+class BaseSerializer(ModelSerializer):
+
+  # 展示类型
+  display_type = None
+
+  # 敏感字段，不会被序列化
+  sensitive_fields = []
+  # 标记作为外键时引用需要排除的字段
+  exclude_foreign = []
+  # 标记作为列表展示时需要排除的字段
+  exclude_list = []
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    for field_name in self.sensitive_fields:
+      self.fields.pop(field_name)
+    if self.display_type == DisplayType.LIST:
+      self._remove_fields(self.exclude_list)
+    if self.display_type == DisplayType.FOREIGN:
+      self._remove_fields(self.exclude_list)
+
+
+
+  def _remove_fields(self, field_names):
+    """移除字段的方法"""
+    for field_name in field_names:
+      self.fields.pop(field_name, None)
